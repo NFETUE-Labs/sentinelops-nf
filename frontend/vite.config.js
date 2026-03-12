20a1cabd41e212910b0dbbe191ea70c0a5aaa639
Nathanael-FETUE <114958970+NathanaelFetue@users.noreply.github.com>
2026-03-12 12:22:09 +0000

    fix: disable Vite HMR in production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    hmr: false,
     host: '0.0.0.0',
     port: 3001,
     allowedHosts: ['app.sentinelops.page'],
