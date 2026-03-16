7913a4cca8dc27fc9551ed4e6710cb1bb87f26f2
Nathanael-FETUE <114958970+NathanaelFetue@users.noreply.github.com>
2026-03-16 07:34:40 +0000

    feat: add Sentry error monitoring to frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,13 @@
+import * as Sentry from "@sentry/react";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+Sentry.init({
+  dsn: "https://8887b61f540d061f01e7ce1f965c5e18@o4511017293971456.ingest.de.sentry.io/4511053000409168",
+  sendDefaultPii: true,
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
